ca6e8719e0453f2a9e5db706e31d31f679c80271
butera-simone <buterasimone@proton.me>
2024-06-13 14:41:44 -0600

    added format options + small fixes

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
-const get = require('.')
+const fetch = require('.')
 
 async function main () {
-  const res = await get('https://api.restful-api.dev/objects/7')
+  const res = await fetch('https://api.restful-api.dev/objects/7')
   console.log(res)
 }
 
