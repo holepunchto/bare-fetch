f7283d28130c84d3dde7e428ba5d57e9a561efab
sethvincent <sethvincent@gmail.com>
2026-01-27 08:01:00 -0800

    add iterable methods to Headers

--- lib/headers.d.ts
+++ lib/headers.d.ts
@@ -4,10 +4,14 @@ interface Headers extends Iterable<[name: string, value: string]> {
   get(name: string): string | null
   has(name: string): boolean
   set(name: string, value: string): void
+  entries(): IterableIterator<[name: string, value: string]>
+  keys(): IterableIterator<string>
+  values(): IterableIterator<string>
+  forEach(callback: (value: string, name: string, headers: Headers) => void, thisArg?: any): void
 }
 
 declare class Headers {
-  constructor(init: Record<string, string> | Iterable<[string, string]>)
+  constructor(init?: Record<string, string> | Iterable<[string, string]>)
 }
 
 export = Headers
