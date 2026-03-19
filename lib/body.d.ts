344b1618c29f40ba3cdeeaaa204c8ec923512f81
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-03-19 10:32:31 +0100

    Import `URLSearchParams`

--- lib/body.d.ts
+++ lib/body.d.ts
@@ -1,6 +1,7 @@
 import { ReadableStream } from 'bare-stream/web'
 import Buffer from 'bare-buffer'
 import { FormData, Blob } from 'bare-form-data'
+import { URLSearchParams } from 'bare-url'
 
 type JSON = string | number | boolean | JSON[] | { [key: string]: JSON }
 
