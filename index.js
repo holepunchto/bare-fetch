6a520db7d3bae281b99d14f236612c9fcc814529
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-04-06 12:19:20 +0200

    Handle premature request closure (#37)

--- index.js
+++ index.js
@@ -77,6 +77,8 @@ module.exports = exports = function fetch(input, init = {}) {
         headers: Object.fromEntries(request._headers)
       },
       (res) => {
+        req.off('close', onclose)
+
         if (request.signal && request.signal.aborted) {
           res.resume()
 
@@ -108,7 +110,7 @@ module.exports = exports = function fetch(input, init = {}) {
       }
     )
 
-    req.on('error', (err) => reject(errors.NETWORK_ERROR('Network error', err)))
+    req.on('error', onerror).on('close', onclose)
 
     try {
       if (request._body) {
@@ -118,6 +120,16 @@ module.exports = exports = function fetch(input, init = {}) {
       req.end()
     } catch (err) {
       req.destroy(err)
+    }
+
+    function onerror(err) {
+      req.off('close', onclose)
+
+      reject(errors.NETWORK_ERROR('Network error', err))
+    }
+
+    function onclose() {
+      onerror(http.errors.CONNECTION_LOST())
     }
   }
 }
