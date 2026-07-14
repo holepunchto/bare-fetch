16fbee77d0fd429df539a986cdd2dcb6898a82c1
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-07-14 14:25:42 +0200

    Add `response.type`

--- lib/response.d.ts
+++ lib/response.d.ts
@@ -2,6 +2,14 @@ import { type HTTPStatusCode, type HTTPStatusMessage } from 'bare-http1'
 import Headers from './headers'
 import Body from './body'
 
+type ResponseType =
+  | 'basic'
+  | 'cors'
+  | 'default'
+  | 'error'
+  | 'opaque'
+  | 'opaqueredirect'
+
 interface ResponseInit {
   status?: HTTPStatusCode
   statusText?: HTTPStatusMessage
@@ -9,6 +17,7 @@ interface ResponseInit {
 }
 
 interface Response extends Body {
+  readonly type: ResponseType
   readonly url: string | null
   readonly redirected: boolean
   readonly status: HTTPStatusCode
